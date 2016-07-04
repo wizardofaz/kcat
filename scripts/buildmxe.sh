280d4a417fe7a1bb46036fef602af79afd95e10a
Robert Cunnings <nw8l@whitemesa.com>
2016-07-04 08:23:05 -0500

    mxe update
      * corrected mxe build scripts

--- scripts/buildmxe.sh
+++ scripts/buildmxe.sh
@@ -1,5 +1,4 @@
-# build file to generate the distribution binary tarball
-autoreconf
+#!/bin/sh
 
 ./configure \
   $PKGCFG \
@@ -9,15 +8,8 @@ autoreconf
   PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
   FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config
 
-make clean
 make
-$PREFIX/bin/i686-pc-mingw32-strip src/kcat.exe
+
+$PREFIX/bin/i686-w64-mingw32.static-strip src/flwkey.exe
 make nsisinst
 mv src/*setup*exe .
-
-make clean
-
-# build the distribution tarball
-./configure
-make distcheck
-make clean
